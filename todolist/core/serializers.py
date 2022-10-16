7e006722c67854ab6f68901f858af966371d2e72
S.P <s.protsenk@gmail.com>
2022-10-16 16:32:09 +0700

    signup logic corrected

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -27,12 +27,14 @@ class SignUpSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         """Check password and password_repeat"""
-        if data.get('password') == data.get('password_repeat'):
+        print(data.get('password'))
+        print(data.get('password_repeat'))
+        if data.get('password') == self.initial_data.get('password_repeat'):
             return data
         raise serializers.ValidationError('Passwords does not match')
 
     def create(self, validated_data):
-        user = self.Meta.model.objects.create(validated_data)
+        user = self.Meta.model.objects.create(**validated_data)
         user.set_password(user.password)
         user.save()
         return user
