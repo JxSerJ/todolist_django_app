7ac239fefc9f657489541df6bffb244655f2f516
S.P <s.protsenk@gmail.com>
2022-10-23 20:31:38 +0700

    goals urls fixed

--- todolist/goals/urls.py
+++ todolist/goals/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from goals import views
 
 urlpatterns = [
-    path('goal_category/create', views.GoalCategoryCreateView.as_view),
-    path('goal_category/list', views.GoalCategoryCreateView.as_view),
-    path('goal_category/<pk>', views.GoalCategoryView.as_view),
+    path('goal_category/create', views.GoalCategoryCreateView.as_view()),
+    path('goal_category/list', views.GoalCategoryCreateView.as_view()),
+    path('goal_category/<pk>', views.GoalCategoryView.as_view()),
 ]
