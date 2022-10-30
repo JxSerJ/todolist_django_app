0851ece3b9e3335ae8992394b7a08cbe74f487f4
S.P <s.protsenk@gmail.com>
2022-10-30 17:15:30 +0700

    models for borad and board-participants added

--- todolist/goals/models.py
+++ todolist/goals/models.py
@@ -5,8 +5,8 @@ from core.models import User
 
 
 class BaseModel(models.Model):
-    created = models.DateTimeField(verbose_name="Дата создания")
-    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
+    created = models.DateTimeField(verbose_name='Дата создания')
+    updated = models.DateTimeField(verbose_name='Дата последнего обновления')
 
     def save(self, *args, **kwargs):
         if not self.id:  # new instance has no id
@@ -18,14 +18,51 @@ class BaseModel(models.Model):
         abstract = True
 
 
+class Board(BaseModel):
+    title = models.CharField(verbose_name='Название', max_length=255)
+    is_deleted = models.BooleanField(verbose_name='Удалена', default=False)
+
+    class Meta:
+        verbose_name = 'Доска'
+        verbose_name_plural = 'Доски'
+
+
+class BoardParticipant(BaseModel):
+    class Role(models.IntegerChoices):
+        owner = 1, 'Владелец'
+        writer = 2, 'Редактор'
+        reader = 3, 'Читатель'
+
+    board = models.ForeignKey(
+        to=Board, verbose_name='Доска', on_delete=models.PROTECT, related_name='participants'
+    )
+    user = models.ForeignKey(
+        to=User, verbose_name='Пользователь', on_delete=models.PROTECT, related_name='participants'
+    )
+    role = models.PositiveSmallIntegerField(
+        verbose_name='Роль', choices=Role.choices, default=Role.owner
+    )
+
+    class Meta:
+        verbose_name = 'Участник'
+        verbose_name_plural = 'Участники'
+
+        constraints = [
+            models.UniqueConstraint(fields=['board', 'user'], name='unique_user_board')
+        ]
+
+
 class GoalCategory(BaseModel):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
-    title = models.CharField(verbose_name="Название", max_length=255)
-    user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
-    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+    title = models.CharField(verbose_name='Название', max_length=255)
+    user = models.ForeignKey(to=User, verbose_name='Автор', on_delete=models.PROTECT)
+    is_deleted = models.BooleanField(verbose_name='Удалена', default=False)
+    board = models.ForeignKey(
+        to=Board, verbose_name='Доска', on_delete=models.PROTECT, related_name='categories'
+    )
 
     def __str__(self):
         return self.title
@@ -33,16 +70,16 @@ class GoalCategory(BaseModel):
 
 class Goal(BaseModel):
     class Status(models.IntegerChoices):
-        to_do = 1, "К выполнению"
-        in_progress = 2, "В процессе"
-        done = 3, "Выполнено"
-        archived = 4, "Архив"
+        to_do = 1, 'К выполнению'
+        in_progress = 2, 'В процессе'
+        done = 3, 'Выполнено'
+        archived = 4, 'Архив'
 
     class Priority(models.IntegerChoices):
-        low = 1, "Низкий"
-        medium = 2, "Средний"
-        high = 3, "Высокий"
-        critical = 4, "Критический"
+        low = 1, 'Низкий'
+        medium = 2, 'Средний'
+        high = 3, 'Высокий'
+        critical = 4, 'Критический'
 
     title = models.CharField(verbose_name='Название', max_length=255)
     description = models.TextField(verbose_name='Описание', null=True, blank=True)
